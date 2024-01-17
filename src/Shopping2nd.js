6a443a52a4f23231d43c2e2ef6f9f90539ecd306
anaishsu <149996609+anaishsu@users.noreply.github.com>
2024-01-17 15:55:29 +0800

    新增shopping-2-3

---
+++ src/Shopping2nd.js
@@ -0,0 +1,176 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Shopping2nd = () => {
+    return (
+        <div>
+            <body className="scrollBox">
+                <main>
+                <section id="shopping2nd">
+        <div className="big-box">
+          <div className="top-steps">
+            <div className="steps">
+              <h3 id="s1">1</h3>
+              <p>購物車</p>
+            </div>
+            <div className="steps">
+              <h3 id="s2">2</h3>
+              <p>選擇配送與付款資訊</p>
+            </div>
+            <div className="steps">
+              <h3 id="s3">3</h3>
+              <p>完成訂單</p>
+            </div>
+          </div>
+          <div className="list-box">
+            <h3 className="title">購物訂單</h3>
+            <div className="list">
+              <p className="products">商品</p>
+              <p className="price">價格</p>
+              <p className="qty">數量</p>
+              <p className="total">商品金額</p>
+            </div>
+
+            <article className="list-item">
+              <figure>
+                <img id="product" src="./images/product1.png" alt="" />
+              </figure>
+              <div className="item">
+                <p>靈魂深處的幽林冒險</p>
+                <p>$2,880</p>
+                <div id="shopping-cart">
+                  <button id="decrease">-</button>
+                  <input type="text" id="quantity" value="1" />
+                  <button id="increase">+</button>
+                </div>
+                <p>$2,880</p>
+                <p>
+                  <Link to=""><img src="./images/garbage.svg" alt="垃圾桶" /></Link>
+                </p>
+              </div>
+            </article>
+            <div className="total">
+              <div className="t1">
+                <ul>
+                  <h4>商品金額：</h4>
+                  <h4>$2,880</h4>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <form action="" name="contact" id="contact_form" method="post">
+            <div className="list-boxes">
+              <h3 className="title">選擇配送方式</h3>
+              <div className="form">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="shipping"
+                    id="shipping-tw"
+                    checked
+                  />
+                  <label className="form-check-label" for="shipping-tw">
+                    台灣-宅配（台灣本島）
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="list-boxes">
+              <h3 className="title">訂購人資料</h3>
+              <div className="form-sign">
+                <div className="sign">
+                  <label for="username">姓名：</label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    title="請輸入姓名"
+                    placeholder="請輸入姓名"
+                    autocomplete="on"
+                    required
+                  />
+                </div>
+                <div className="sign">
+                  <label for="tel">電話：</label>
+                  <input
+                    type="tel"
+                    name="tel"
+                    id="tel"
+                    title="請輸入連絡電話"
+                    placeholder="請輸入號碼"
+                    autocomplete="on"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="notice-box">
+                <p>
+                  此表單僅供示意，無蒐集資料之功能，不會將資料提供給第三人、或移作其他目的使用。
+                </p>
+              </div>
+            </div>
+            <div className="list-boxes">
+              <h3 className="title">付款方式</h3>
+              <div className="form">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="paying"
+                    id="paying-card"
+                  />
+                  <label className="form-check-label" for="paying-card">
+                    信用卡一次付清
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="paying"
+                    id="paying-later"
+                    checked
+                  />
+                  <label className="form-check-label" for="paying-later">
+                    貨到付款
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="list-boxes">
+              <h3 className="title">發票資訊</h3>
+              <div className="form">
+                <label for="regularSelect"></label>
+                <select id="regularSelect" name="regularSelect">
+                  <option value="1">二聯式電子發票(存入會員帳號)</option>
+                  <option value="2">其他</option>
+                </select>
+              </div>
+            </div>
+            <div className="next-box">
+              <div className="btn">
+                <Link to="./shopping.html"><button type="button">返回前頁</button></Link>
+              </div>
+              
+              <div className="btn" ref={sthRef} onClick={handleSubmit}>
+                <input type="submit" id="submit" value="確認結帳"/>
+                
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+                </main>
+            </body>
+        </div>
+
+    )
+}
+const handleSubmit = (e)=>{
+  let sthValue = sthRef.current.value;
+  localStorage.setItem("key", sthValue);
+  navigate("./shopping3rd.html");
+}
+localStorage.clear()
+export default Shopping2nd;
