85a64cfc70fff5194e25d5b336786a5eeafee198
anaishsu <149996609+anaishsu@users.noreply.github.com>
2024-02-05 17:57:57 +0800

    0205

--- src/Footer.js
+++ src/Footer.js
@@ -1,45 +1,66 @@
 import React from "react";
-import {Link} from "react-router-dom"
-const Footer = ()=>{
-    return (
-            <footer>
-                <div>
-                    <div className="footer-box">
-                        <ul>
-                            <li><h3>客服中心</h3></li>
-                            <li><Link to="#">常見問題</Link></li>
-                            <li><Link to="#">運送服務</Link></li>
-                            <li><Link to="#">退換貨服務</Link></li>
-                        </ul>
-                    </div>
-                    <div className="footer-box">
-                        <ul>
-                            <li><h3>分店查詢</h3></li>
-                            <li><Link to="#">搜尋櫃點</Link></li>
-                            <li><Link to="#">線上服務</Link></li>
-                            <li><Link to="#">聯絡方式</Link></li>
-                        </ul>
-                    </div>
-                    <div className="footer-box">
-                        <ul>
-                            <li><h3>關於我們</h3></li>
-                            <li><Link to="#">關於Aroma</Link></li>
-                            <li><Link to="#">加入我們</Link></li>
-                            <li><Link to="#">隱私權政策</Link></li>
-                        </ul>
-                    </div>
-                    <div className="footer-box">
-                        <ul>
-                            <li><h3>了解更多</h3></li>
-                            <li><Link to="#">Instagram</Link></li>
-                            <li><Link to="#">Facebook</Link></li>
-                            <li><Link to="#">LINE</Link></li>
-                        </ul>
-                    </div>
-                </div>
-            </footer>
-
-    )
-}
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div className="footer-box">
+          <ul>
+            <li>
+            <Link to="/" className="item-logo">
+              <img src="./icon/logo-grey.svg" alt="LOGO" />
+            </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-box">
+          <ul>
+            <li>
+              <h3>聯絡我們</h3>
+            </li>
+            <li>
+                
+              <p>110 台北市花香二路28號1樓</p>
+            </li>
+            <li>
+              <p>+886 02 8788 8888</p>
+            </li>
+            <li>
+              <p>週一-週日 11:00-21:30</p>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-box">
+          <ul>
+            <li>
+              <h3>客服專區</h3>
+            </li>
+            <li>
+              <Link to="#">有什麼想詢問的嗎？</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-box">
+          <ul>
+            <li>
+              <h3>追蹤我們</h3>
+            </li>
+            <div className="item">
+              <Link to="#">
+                <img src="./icon/icon_instagram.svg" alt="instagram" />
+              </Link>
+              <Link to="#">
+                <img src="./icon/icon_facebook.svg" alt="facebook" />
+              </Link>
+              <Link to="#">
+                <img src="./icon/icon_youtube.svg" alt="youtube" />
+              </Link>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
