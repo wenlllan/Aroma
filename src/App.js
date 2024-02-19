b415cc7a3d183c52ca04b46fef492d6b5230a76e
wenlllan <wenlllan33@gmail.com>
2024-02-19 18:05:02 +0800

    1234

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React,{useState,useEffect} from "react";
 import "../src/css/style.css";
 import Layout from "./Layout"
-import Homepage from "./Homepage";
 import ProdDream from "./ProdDream";
 import ProdSummer from "./ProdSummer";
 import ProdSoul from "./ProdSoul";
